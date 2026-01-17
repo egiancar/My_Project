6c406bc7a629a43136faaee4422b468345e97845
egiancar <e.giancar7@gmail.com>
2026-01-16 20:51:55 -0500

    Proyecto v2 script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # My_Project
-My Test Project
+My Test Project Sprint 7
+
+La Applicacion web tenenmos un boton para escrbir un mensaje
+    crear un histograma
+    mostrar un grafico plotly interactivo
---
+++ app.py
@@ -0,0 +1,25 @@
+import plotly.express as px
+import streamlit as st
+import pandas as pd
+
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+hist_button = st.button('Construir histograma')  # crear un botón
+
+# Encabezado de la aplicación
+st.header('Análisis de anuncios de vehículos usados')
+
+if hist_button:
+    # escribir un mensaje
+    st.write('Creación de un histograma para la columna odómetro')
+    fig = px.histogram(car_data, x="odometer")  # crear un histograma
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+# Botón para construir gráfico de dispersión
+scatter_button = st.button('Construir gráfico de dispersión')
+
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión entre odómetro y precio')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
